14c554207e6a29f5cd81f281c53026c2f765ec0c
Seán Hester <sean.hester@zinkworks.com>
2023-08-31 14:28:52 +0100

    Put MD5 method in it's own class

--- src/main/java/service/UrlService.java
+++ src/main/java/service/UrlService.java
@@ -38,7 +38,7 @@ public class UrlService {
     // Create
     // Add a new short URL. MD5 conversion will go here (I think)
     public void addNewUrl(String originalUrl) {
-        MD5Hash(originalUrl);
+        MD5Hash.MD5HashingMethod(originalUrl);
 
         Optional<BountyUrlTable> shortenedUrl = repositoryUrl.findByShortUrl(bountyUrlTable.getShortUrl());
         if (shortenedUrl.isPresent()) {
@@ -48,26 +48,7 @@ public class UrlService {
         // and short url to repository class.
     }
 
-    private static String MD5Hash(String originalUrl) {
-        String finalShortUrl = "";
-        try {
-            MessageDigest md;
-            md = MessageDigest.getInstance("MD5"); // Create an instance of Message Digest. Message Digests are secure one-way hash functions
-            // it outputs a fixed length hash value.
-            // 'getInstance' returns a message digest object which implements the specified algorithm (md5)
-            byte[] inputBytes = originalUrl.getBytes();    // convert original url to bytes
-            md.update(inputBytes);                         // update the message digest with the input bytes
-            byte[] md5Bytes = md.digest();                 // Calculate the MD5 hash, returns an array of bytes 'md5Bytes'
-            // digest() performs final hashing computations.
 
-            String base64Encoded = Base64.getEncoder().encodeToString(md5Bytes);   // encodes the byte array 'md5Bytes' to base64
-            finalShortUrl = base64Encoded.substring(0, 7);    // Substring of base64Encoded with first 7 characters
-
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        return finalShortUrl;    // output the final short Url string
-    }
         
 
 
