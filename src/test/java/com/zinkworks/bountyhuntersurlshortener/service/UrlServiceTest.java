247b66c672ac04c1c8e324df0afeac1d32a5b04b
apprentices1-admin <conor.lynam@zinkworks.com>
2023-09-08 15:38:57 +0100

    Testing bugs and fun stuff

--- src/test/java/com/zinkworks/bountyhuntersurlshortener/service/UrlServiceTest.java
+++ src/test/java/com/zinkworks/bountyhuntersurlshortener/service/UrlServiceTest.java
@@ -5,6 +5,7 @@ import com.zinkworks.bountyhuntersurlshortener.model.BountyUrlTable;
 import com.zinkworks.bountyhuntersurlshortener.repository.RepositoryUrl;
 import org.apache.catalina.LifecycleState;
 import org.checkerframework.checker.calledmethods.qual.RequiresCalledMethods;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
@@ -14,6 +15,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -24,25 +26,28 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 
-@ExtendWith(MockitoExtension.class)
+
+@SpringBootTest
 class UrlServiceTest {
 
-    @InjectMocks
-    UrlService urlService;
     @Autowired
     private RepositoryUrl repositoryUrl;
 
+    private UrlService urlService;
 
 
-//    @Test
-//    void canFindOriginalUrlByShortUrl() throws UrlNotFoundException {
-//
-//        when(urlService.findOriginalUrl("HuuMy3K"))
-//                .thenReturn("https://www.youtube.com/watch?v=SDwqcFwvwY0");
-//
-//    }
 
     @Test
+    @DisplayName("Testing for expected value")
+    void canFindOriginalUrlByShortUrl() throws UrlNotFoundException {
+
+        when(urlService.findOriginalUrl("HuuMy3K"))
+                .thenReturn("https://www.youtube.com/watch?v=SDwqcFwvwY0");
+
+    }
+
+    @Test
+    @DisplayName("Testing for all Urls")
     void getAllUrlsTest(){
         BountyUrlTable testTable = BountyUrlTable.builder()
                 .originalUrl("https://www.youtube.com/watch?v=SDwqcFwvwY0")
