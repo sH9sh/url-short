43b7384c146fc0ac4e0488da4477cfa96b032cd8
Jessica Hoey <jessica.hoey@zinkworks.com>
2023-09-08 14:41:40 +0100

    latest changes

--- src/main/java/com/zinkworks/bountyhuntersurlshortener/service/BlackList.java
+++ src/main/java/com/zinkworks/bountyhuntersurlshortener/service/BlackList.java
@@ -26,7 +26,21 @@ public class BlackList {
     }
 
 //Need to add a keywords check, can comment out the url constraint if no longer needed or get program to check both simultaneously.
-
+public static boolean checkBlacklistKeywords(String originalURL) throws FileNotFoundException{
+    List<String> blacklistKeywords = new ArrayList<>();
+    Scanner scnr = new Scanner(new FileReader("urls.txt"));
+    String str;
+    while (scnr.hasNext()) {
+        str = scnr.next();
+        blacklistKeywords.add(str);
+    }
+    for (String substring : blacklistKeywords) {
+        if (originalURL.contains(substring)) {
+            return true;
+        }
+    }
+    return false;
+}
 
 }
 
