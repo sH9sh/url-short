68ff3cb0159b8f3e090d741396f77f473d20c103
Jessica Hoey <jessica.hoey@zinkworks.com>
2023-09-13 09:06:07 +0100

    blacklist

--- src/main/java/com/zinkworks/bountyhuntersurlshortener/service/BlackList.java
+++ src/main/java/com/zinkworks/bountyhuntersurlshortener/service/BlackList.java
@@ -9,32 +9,32 @@ import java.util.Scanner;
 public class BlackList {
 
 
-    public static boolean checkBlackList(String originalURL) throws FileNotFoundException{
-        List<String> blackList = new ArrayList<>();
-        Scanner scnr = new Scanner(new FileReader("urls.txt"));
-        String str;
-        while (scnr.hasNext()) {
-            str = scnr.next();
-            blackList.add(str);
-        }
-        for (String substring : blackList) {
-            if (originalURL.contains(substring)) {
-                return true;
-            }
-        }
-        return false;
-    }
+//    public static boolean checkBlackList(String originalURL) throws FileNotFoundException{
+//        List<String> blackList = new ArrayList<>();
+//        Scanner scnr = new Scanner(new FileReader("urls.txt"));
+//        String str;
+//        while (scnr.hasNext()) {
+//            str = scnr.next();
+//            blackList.add(str);
+//        }
+//        for (String substring : blackList) {
+//            if (originalURL.contains(substring)) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
 
 //Need to add a keywords check, can comment out the url constraint if no longer needed or get program to check both simultaneously.
-public static boolean checkBlacklistKeywords(String originalURL) throws FileNotFoundException{
-    List<String> blacklistKeywords = new ArrayList<>();
+public static boolean checkBlackList(String originalURL) throws FileNotFoundException{
+    List<String> blackList = new ArrayList<>();
     Scanner scnr = new Scanner(new FileReader("urls.txt"));
     String str;
     while (scnr.hasNext()) {
         str = scnr.next();
-        blacklistKeywords.add(str);
+        blackList.add(str);
     }
-    for (String substring : blacklistKeywords) {
+    for (String substring : blackList) {
         if (originalURL.contains(substring)) {
             return true;
         }
