8d10c5e1fce2b381e94a1c6a1072a611b4c1b0d3
apprentices1-admin <conor.lynam@zinkworks.com>
2023-09-14 10:47:00 +0100

    Service Tests

--- src/test/java/com/zinkworks/bountyhuntersurlshortener/service/UrlServiceTest.java
+++ src/test/java/com/zinkworks/bountyhuntersurlshortener/service/UrlServiceTest.java
@@ -35,8 +35,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 class UrlServiceTest {
 
-    @InjectMocks
-    private ControllerUrl controllerUrl;
+
     @Mock
     private RepositoryUrl repositoryUrl;
 
@@ -46,12 +45,20 @@ class UrlServiceTest {
 
 
     @Test
-    @DisplayName("Testing for expected value")
-    void canFindOriginalUrlByShortUrl() throws UrlNotFoundException {
+    @DisplayName("Testing for expected value with valid url")
+    void canFindOriginalUrlByShortUrlWithUrlStoredInDatabase() throws UrlNotFoundException {
 
         when(urlService.findOriginalUrl("HuuMy3K"))
                 .thenReturn("https://www.youtube.com/watch?v=SDwqcFwvwY0");
 
+    }
+
+    @Test
+    @DisplayName("Testing for exception with invalid url")
+    void findOriginalUrlThrowExceptionWithUrlNotOnDatabase() throws UrlNotFoundException {
+        String shortUrl = "gdsav5p";
+        when(urlService.findOriginalUrl(shortUrl))
+                .thenThrow( new UrlNotFoundException("No entity with " + shortUrl + " found."));
     }
 
     @Test
@@ -64,17 +71,20 @@ class UrlServiceTest {
         assertThat(urlList).isNotNull();
 
         assertThat(urlList.size()).isEqualTo(1);
-
     }
 
     @Test
     @DisplayName("Testing to see if a short url is created ")
-    void createNewShortUrl() throws MalformedURLException, BlackListedUrlException, FileNotFoundException, InvalidUrlException {
+    void createNewShortUrlValidUrl() throws MalformedURLException, BlackListedUrlException, FileNotFoundException, InvalidUrlException {
         when(urlService.addNewUrl("https://www.youtube.com/"))
                 .thenReturn("0xE0O6P");
+    }
 
-
-
+    @Test
+    @DisplayName("Testing for exception when url is blacklisted")
+    void createNewShortUrlBlacklistedUrl() throws MalformedURLException, BlackListedUrlException, FileNotFoundException, InvalidUrlException {
+        when(urlService.addNewUrl("https://www.cunt.com/"))
+                .thenThrow(new BlackListedUrlException("The url you entered is on our blacklist."));
     }
 
 }
