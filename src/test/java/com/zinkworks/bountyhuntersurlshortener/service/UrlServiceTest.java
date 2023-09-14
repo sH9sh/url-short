5774cd65551466b69e155828c58c26eaad2b44aa
apprentices1-admin <conor.lynam@zinkworks.com>
2023-09-14 13:20:23 +0100

    Service tests

--- src/test/java/com/zinkworks/bountyhuntersurlshortener/service/UrlServiceTest.java
+++ src/test/java/com/zinkworks/bountyhuntersurlshortener/service/UrlServiceTest.java
@@ -1,31 +1,18 @@
 package com.zinkworks.bountyhuntersurlshortener.service;
 
-import com.zinkworks.bountyhuntersurlshortener.controller.ControllerUrl;
 import com.zinkworks.bountyhuntersurlshortener.exceptions.BlackListedUrlException;
 import com.zinkworks.bountyhuntersurlshortener.exceptions.InvalidUrlException;
 import com.zinkworks.bountyhuntersurlshortener.exceptions.UrlNotFoundException;
 import com.zinkworks.bountyhuntersurlshortener.model.BountyUrlTable;
 import com.zinkworks.bountyhuntersurlshortener.repository.RepositoryUrl;
-import org.apache.catalina.LifecycleState;
-import org.checkerframework.checker.calledmethods.qual.RequiresCalledMethods;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
+import org.springframework.web.util.UriComponentsBuilder;
 import java.io.FileNotFoundException;
 import java.net.MalformedURLException;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -43,6 +30,13 @@ class UrlServiceTest {
     private UrlService urlService;
 
 
+
+    @Test
+    @DisplayName("Testing to see if a short url is created ")
+    void createNewShortUrlValidUrl() throws MalformedURLException, BlackListedUrlException, FileNotFoundException, InvalidUrlException {
+        when(urlService.addNewUrl("https://www.youtube.com/"))
+                .thenReturn("0xE0O6P");
+    }
 
     @Test
     @DisplayName("Testing for expected value with valid url")
@@ -64,26 +58,42 @@ class UrlServiceTest {
     @DisplayName("Testing for all Urls")
 
     void getAllUrlsTest(){
-
         List<BountyUrlTable> urlList = repositoryUrl.findAll();
-
         assertThat(urlList).isNotNull();
-
         assertThat(urlList.size()).isEqualTo(1);
     }
 
-    @Test
-    @DisplayName("Testing to see if a short url is created ")
-    void createNewShortUrlValidUrl() throws MalformedURLException, BlackListedUrlException, FileNotFoundException, InvalidUrlException {
-        when(urlService.addNewUrl("https://www.youtube.com/"))
-                .thenReturn("0xE0O6P");
-    }
+
+
+
 
     @Test
     @DisplayName("Testing for exception when url is blacklisted")
     void createNewShortUrlBlacklistedUrl() throws MalformedURLException, BlackListedUrlException, FileNotFoundException, InvalidUrlException {
         when(urlService.addNewUrl("https://www.cunt.com/"))
                 .thenThrow(new BlackListedUrlException("The url you entered is on our blacklist."));
+    }
+
+    @Test
+    @DisplayName("Testing for exception when url is not valid")
+    void createNewShortUrlInvalidUrl() throws MalformedURLException, BlackListedUrlException, FileNotFoundException, InvalidUrlException {
+        when(urlService.addNewUrl("invalid information"))
+                .thenThrow(new MalformedURLException());
+    }
+
+    @Test
+    @DisplayName("Testing to see if validation method will remove sql injection query")
+    void createSanatizedUrl(){
+        String sqlInjectedUrl = "https://insecure-website.com/products?category=Gifts'+OR+1=1--";
+
+        String sanatizedUrl = UriComponentsBuilder.fromHttpUrl(sqlInjectedUrl)
+                .replaceQuery(null)
+                .build()
+                .toUriString();
+
+        assertThat(sanatizedUrl).isNotNull();
+        assertThat(sanatizedUrl.length()).isEqualTo(37);
+
     }
 
 }
