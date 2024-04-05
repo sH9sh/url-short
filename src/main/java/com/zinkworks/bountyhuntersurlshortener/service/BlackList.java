200a8a1e508533e209004907f45039b13517f1bc
Conor Lynam <conor.lynam@zinkworks.com>
2024-04-05 12:47:40 +0000

    BlackList.java edited online with Bitbucket

--- src/main/java/com/zinkworks/bountyhuntersurlshortener/service/BlackList.java
+++ src/main/java/com/zinkworks/bountyhuntersurlshortener/service/BlackList.java
@@ -8,21 +8,27 @@ import java.util.Scanner;
 
 public class BlackList {
 
-public static boolean checkBlackList(String originalURL) throws FileNotFoundException{
-    List<String> blackList = new ArrayList<>();
-    Scanner scnr = new Scanner(new FileReader("src/main/resources/keywordsForBlacklist.txt"));
-    String str;
-    while (scnr.hasNext()) {
-        str = scnr.next();
-        blackList.add(str);
-    }
-    for (String substring : blackList) {
-        if (originalURL.contains(substring)) {
-            return true;
+    public static boolean checkBlackList(String originalURL) throws FileNotFoundException {
+        List<String> blackList = new ArrayList<>();
+        // Load the file from the classpath
+        InputStream inputStream = BlackList.class.getResourceAsStream("/keywordsForBlacklist.txt");
+        if (inputStream == null) {
+            throw new FileNotFoundException("keywordsForBlacklist.txt not found in classpath");
         }
+        Scanner scanner = new Scanner(inputStream);
+        while (scanner.hasNext()) {
+            String str = scanner.next();
+            blackList.add(str);
+        }
+        scanner.close();
+
+        for (String substring : blackList) {
+            if (originalURL.contains(substring)) {
+                return true;
+            }
+        }
+        return false;
     }
-    return false;
-}
 
 }
 
