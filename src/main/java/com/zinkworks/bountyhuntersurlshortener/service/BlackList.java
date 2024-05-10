5bdc4ceb00db61c48e8113542f7a68db12e9e451
Seán Hester <sean.hester@zinkworks.com>
2024-05-10 14:22:53 +0100

    ss

--- src/main/java/com/zinkworks/bountyhuntersurlshortener/service/BlackList.java
+++ src/main/java/com/zinkworks/bountyhuntersurlshortener/service/BlackList.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class BlackList {
-// comment more ddd ds
+// comment more ddd ds ddddd
     public static boolean checkBlackList(String originalURL) throws FileNotFoundException {
         List<String> blackList = new ArrayList<>();
         // Load the file from the classpath
